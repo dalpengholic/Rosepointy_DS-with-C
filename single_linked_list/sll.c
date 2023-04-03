c010859bcb1b8c04bf6f3d068edd2a5640194369
dalpengholic <dalpengholic@gmail.com>
2023-04-03 14:21:11 +0300

    first commit

---
+++ single_linked_list/sll.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Functions 
+// InitList() / ReleaseList()
+// PrintList()
+// FindNode()
+// DeletNode()
+// InsertAtHead()/ InsertAtTail()
+// GetLength()
+
+typedef struct NODE {
+    // data
+    int nData;
+    // pointer to manage data
+    struct NODE* next;
+} NODE;
+
+
+int main(){
+    return 0;
+}
