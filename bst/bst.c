4c61a0bf73ecf3a767f4b8a1e79c2cf2696f2d23
dalpengholic <dalpengholic@gmail.com>
2023-04-13 15:59:44 +0300

    BST.230413.AddNode using recursive way

--- bst/bst.c
+++ bst/bst.c
@@ -34,41 +34,90 @@ void PrintTree(NODE* parentNode){
 }
 
 // 재귀호출없이 while, break로 진행 
-void InsertNode(const char* pszData){
-    NODE* pNewNode = (NODE*)malloc(sizeof(NODE));
-    memset(pNewNode, 0, sizeof(NODE));
-    strcpy(pNewNode->szData, pszData);
-    if (g_Root == NULL){
-        g_Root = pNewNode;
-    }
-    else {
-        NODE* pCurrent = g_Root;
-        while(pCurrent != NULL){
-            if(strcmp(pCurrent->szData, pszData) < 0)
-                if (pCurrent->right != NULL)
-                    pCurrent = pCurrent->right;
-                else{
-                    pCurrent->right = pNewNode;
-                    break;
-                }
-            else{
-                if (pCurrent->left != NULL)
-                    pCurrent = pCurrent->left;
-                else{
-                    pCurrent->left = pNewNode;
-                    break;
-                    }   
-            }         
-        }
-    }
-    g_nSize++;
-}
+// void InsertNode(const char* pszData){
+//     NODE* pNewNode = (NODE*)malloc(sizeof(NODE));
+//     memset(pNewNode, 0, sizeof(NODE));
+//     strcpy(pNewNode->szData, pszData);
+//     if (g_Root == NULL){
+//         g_Root = pNewNode;
+//     }
+//     else {
+//         NODE* pCurrent = g_Root;
+//         while(pCurrent != NULL){
+//             if(strcmp(pCurrent->szData, pszData) < 0)
+//                 if (pCurrent->right != NULL)
+//                     pCurrent = pCurrent->right;
+//                 else{
+//                     pCurrent->right = pNewNode;
+//                     break;
+//                 }
+//             else{
+//                 if (pCurrent->left != NULL)
+//                     pCurrent = pCurrent->left;
+//                 else{
+//                     pCurrent->left = pNewNode;
+//                     break;
+//                     }   
+//             }         
+//         }
+//     }
+//     g_nSize++;
+// }
 // 45min
-void FindNode(){
 
+//재귀호출 사용. 
+NODE* AddNode(NODE* pStartNode, const char* pszData){
+    //재귀호출 멈추는 조건. 새노드 추가 및 초기화
+    if (pStartNode == NULL){
+        // 아래에서 NODE* pStartNode선언하여 에러발생!!
+        pStartNode = (NODE*)malloc(sizeof(NODE));
+        memset(pStartNode, 0, sizeof(NODE));
+        strcpy(pStartNode->szData, pszData);
+        g_nSize++;
+    }
+    else{
+        if(strcmp(pStartNode->szData, pszData) < 0)
+            pStartNode->right = AddNode(pStartNode->right, pszData);
+        else
+            pStartNode->left = AddNode(pStartNode->left, pszData);
+    }
+
+    return pStartNode;
 }
+
+
+
+NODE* FindNode(const char* pszData){
+    if (g_Root == NULL){
+        printf("No Tree\n");
+        return NULL;
+    }
+    else{
+        NODE* pCurrent = g_Root;
+        while (pCurrent != 0){
+            if(strcmp(pCurrent->szData, pszData) == 0){
+                printf("Found %p, %s\n",pCurrent, pCurrent->szData);
+                return pCurrent;
+            }
+
+            else{
+                if(strcmp(pCurrent->szData, pszData) < 0)
+                    pCurrent = pCurrent->right;
+                else
+                    pCurrent = pCurrent->left;
+            }
+        }
+
+    }
+}
+//63min
+
 
 void DeleteNode(){
+    //Max or min first
+    //No child
+    //One child
+    //Two children
 
 }
 
@@ -78,18 +127,37 @@ void GetSize(){
 
 
 int main(){
-    InsertNode("5-TEST");
+    // InsertNode("5-TEST");
+    // PrintTree(g_Root);
+    // puts("***********");
+    // InsertNode("4-TEST");
+    // InsertNode("7-TEST");
+    // PrintTree(g_Root);
+    // puts("***********");
+    // InsertNode("6-TEST");
+    // InsertNode("8-TEST");
+    // PrintTree(g_Root);
+    // puts("***********");
+
+    // FindNode("8-TEST");
+    // FindNode("8-TE");
+    // FindNode("6-TEST");
+    // ReleaseTree(g_Root);
+    g_Root = AddNode(g_Root, "15-Test");
+    printf("%p\n",g_Root);
+    AddNode(g_Root, "18-Test");
+    printf("%p\n",g_Root);
+    AddNode(g_Root, "13-Test");
     PrintTree(g_Root);
     puts("***********");
-    InsertNode("4-TEST");
-    InsertNode("7-TEST");
+    g_Root = AddNode(g_Root, "12-Test");
+    g_Root = AddNode(g_Root, "16-Test");
+    g_Root = AddNode(g_Root, "17-Test");
     PrintTree(g_Root);
-    puts("***********");
-    InsertNode("6-TEST");
-    InsertNode("8-TEST");
-    PrintTree(g_Root);
-    puts("***********");
     ReleaseTree(g_Root);
+
+
+    
     return 0;
 }
 
